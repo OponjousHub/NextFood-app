9707f73d9b53a91b55fea4ce878504b348ac8a57
joe.ikdura@gmail.com <joe.ikdura@gmail.com>
2024-05-16 14:22:53 +0100

    completed the slide show on the home page

--- components/slideShow.js
+++ components/slideShow.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Image from "next/image";
 import burger from "@/assets/burger.jpg";
 import curry from "@/assets/curry.jpg";
@@ -6,26 +8,41 @@ import pizza from "@/assets/pizza.jpg";
 import macncheese from "@/assets/macncheese.jpg";
 import schnitzel from "@/assets/schnitzel.jpg";
 import tomato_salad from "@/assets/tomato_salad.jpg";
-
+import { useEffect, useState } from "react";
+import classes from "./slideShow.module.css";
 const images = [
-  { image: burger, alt: "burger recipe" },
-  { image: curry, alt: "burger curry" },
-  { image: dumplings, alt: "dumplings recipe" },
-  { image: pizza, alt: "pizza recipe" },
-  { image: macncheese, alt: "macncheese recipe" },
-  { image: schnitzel, alt: "schnitzel recipe" },
-  { image: tomato_salad, alt: "tomato_salad recipe" },
+  { image: burger, alt: "A delicious, juicy burger" },
+  { image: curry, alt: "A delicious, spicy curry" },
+  { image: dumplings, alt: "Steamed dumplings" },
+  { image: pizza, alt: "A delicious pizz" },
+  { image: macncheese, alt: "Mac and cheese" },
+  { image: schnitzel, alt: "A delicious schnitzel" },
+  { image: tomato_salad, alt: "A delicious tomato salad" },
 ];
 
 function SlideShow() {
-  console.log(images);
+  const [displayedImgIndex, setDisplayedImgIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDisplayedImgIndex((prevIndex) =>
+        prevIndex < images.length - 1 ? prevIndex + 1 : 0
+      );
+    }, 2000);
+
+    return () => clearInterval(interval);
+  }, []);
+
   return (
-    <div>
-      <ul>
-        {images.map((image, index) => (
-          <Image key={index} src={images.image} alt={images.alt} />
-        ))}
-      </ul>
+    <div className={classes.slide}>
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          src={image.image}
+          alt={image.alt}
+          className={index === displayedImgIndex ? classes.active : ""}
+        />
+      ))}
     </div>
   );
 }
