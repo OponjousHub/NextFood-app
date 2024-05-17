732cbee1f670a7f77209156e6694ed7058f0d5e5
joe.ikdura@gmail.com <joe.ikdura@gmail.com>
2024-05-17 14:56:07 +0100

    Added an NOt Found page

---
+++ app/not-found.js
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div className="not-found">
+      <h2>Not Found!</h2>
+      <p>Unfortunately we could not found the requested page or resource.</p>
+    </div>
+  );
+}
