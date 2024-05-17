68b52233cc9e2591e8fcc4529675c696a5fec521
joe.ikdura@gmail.com <joe.ikdura@gmail.com>
2024-05-17 10:10:04 +0100

    install some sql packages

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,20 +1,28 @@
 import Link from "next/link";
+import MealGrid from "../../components/meals/mealgrid";
+import classes from "./meal_page.module.css";
 
 function meals() {
   return (
-    <main>
-      <h1>This is meals Page</h1>
-
-      <p>
-        <Link href="meals/item-1">Food 1</Link>
-      </p>
-      <p>
-        <Link href="meals/item-2">Food 2</Link>
-      </p>
-      <p>
-        <Link href="meals/item-3">Food 3</Link>
-      </p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <h1 className={classes.header_text}>
+          Delicious meals, created <span>by you</span>
+        </h1>
+        <div className={classes.cta_text}>
+          <p>
+            Choose your favourite recipe and cook it your self. It is easy and
+            fun!
+          </p>
+          <Link href="/meals/shear" className={classes.cta}>
+            Shear Your Favorite Recipe
+          </Link>
+        </div>
+      </header>
+      <main>
+        <MealGrid />
+      </main>
+    </>
   );
 }
 
