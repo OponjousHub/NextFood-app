ea4749922800f4fa4780b29112d25e8a13ea8101
joe.ikdura@gmail.com <joe.ikdura@gmail.com>
2024-05-17 14:09:52 +0100

    Added an error page

---
+++ app/meals/error.js
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Error() {
+  return (
+    <main className="error">
+      <h2>An error occured!</h2>
+      <p>Could not fetch meal. Please try again later</p>
+    </main>
+  );
+}
