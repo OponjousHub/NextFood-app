6f7aaeeff75be835e1b8ad9b95f9e9c3b0af2ab3
joe.ikdura@gmail.com <joe.ikdura@gmail.com>
2024-05-17 15:30:31 +0100

    Added meal detail page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,24 @@
-function DisplayPage() {
+import classes from "./mealSlug.module.css";
+import Image from "next/image";
+
+function DisplayMealPage() {
   return (
-    <main>
-      <h1>This is the food details page</h1>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image fill />
+          <div className={classes.header_text}>
+            <h1>Title</h1>
+            <p className={classes.creator}>
+              By <a href={`mailto:${"EMAIL"}`}>NAME</a>
+            </p>
+            <p>SUMMARY</p>
+          </div>
+        </div>
+      </header>
+      <main></main>
+    </>
   );
 }
 
-export default DisplayPage;
+export default DisplayMealPage;
